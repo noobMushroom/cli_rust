302b8fb22141df4775b8dfbe23d4d6d2587e5a42
mushroom <mushroom019.coder@gmail.com>
2024-07-19 20:41:48 +0530

    added echo command in rust

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{command, Arg, ArgAction};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(Arg::new("text"))
+        .arg(
+            Arg::new("omit-newline")
+                .short('n')
+                .long("omit-newline")
+                .required(false)
+                .action(ArgAction::SetFalse),
+        )
+        .get_matches();
+
+    if let Some(text) = matches.get_one::<String>("text") {
+        if let Some(value) = matches.get_one::<bool>("omit-newline") {
+            if *value {
+                println!("{}", text);
+            } else {
+                print!("{}", text);
+            }
+        }
+    }
 }
